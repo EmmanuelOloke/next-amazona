24319ba21e725af70a44772934954bff5ddfc2e6
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-04-19 23:35:46 +0100

    trying to fix material ui errors

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import useStyles from '../../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
 
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Amazona</Typography>
         </Toolbar>
